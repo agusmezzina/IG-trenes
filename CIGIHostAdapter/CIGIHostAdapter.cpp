70c9e58e331466af25825507227eb4e1624f6a88
Agustín Mezzina <apmezzina@gmail.com>
2016-06-03 19:03:29 -0300

    Software control terminado

--- CIGIHostAdapter/CIGIHostAdapter.cpp
+++ CIGIHostAdapter/CIGIHostAdapter.cpp
@@ -3,14 +3,23 @@
 
 #include "stdafx.h"
 #include "CigiHost.h"
+#include "ModelUpdater.h"
 #include "SceneData.h"
+#include <thread>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//UDPServer server;
 	SceneData data;
 	CigiHost server(&data);
-	server.run();
+	ModelUpdater model(&data);
+	
+	std::thread cigiThread(&CigiHost::run, &server);
+	std::thread modelThread(&ModelUpdater::run, &model);
+
+	cigiThread.join();
+	modelThread.join();
+
 	return 0;
 }
 
---
+++ CIGIHostAdapter/ModelUpdater.cpp
@@ -0,0 +1,55 @@
+#include "ModelUpdater.h"
+#include <boost\array.hpp>
+#include <boost\asio.hpp>
+#include <boost\algorithm\string.hpp>
+#include <iostream>
+
+using boost::asio::ip::udp;
+
+ModelUpdater::ModelUpdater(SceneData* data)
+{
+	this->data = data;
+}
+
+void ModelUpdater::run()
+{
+	boost::asio::io_service io_service;
+	udp::socket socket(io_service, udp::endpoint(udp::v4(), 8887));
+
+	for (;;)
+	{
+		boost::array<unsigned char, 32768> recv_buf;
+		udp::endpoint remote_endpoint;
+		boost::system::error_code error;
+
+		std::size_t len = socket.receive_from(boost::asio::buffer(recv_buf),
+			remote_endpoint,
+			0,
+			error);
+		if (error && error != boost::asio::error::message_size)
+			throw boost::system::system_error(error);
+
+		double x, y, z = 0;
+		std::string msg(reinterpret_cast<char*>(recv_buf.c_array()), len);
+		if (msg.back() == '\f'){
+			msg.pop_back();
+			std::vector<std::string> fields;
+			boost::split(fields, msg, boost::is_any_of(";"));
+			if (fields.size() == 4)
+			{
+				x = std::stod(fields[0]);
+				y = std::stod(fields[1]);
+				z = std::stod(fields[2]);
+			}
+			data->setData(x, y, z);
+		}
+		std::cout << x << std::endl;
+		std::cout << y << std::endl;
+		std::cout << z << std::endl;
+	}
+	socket.close();
+}
+
+ModelUpdater::~ModelUpdater()
+{
+}
